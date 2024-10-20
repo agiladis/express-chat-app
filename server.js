9390c68c7285e97947d5ab28a4ce0d8c9f9896f8
Agil Adi Saputro <agiladisaputro@gmail.com>
2024-10-20 23:55:53 +0700

    [ADD] simple connection

--- server.js
+++ server.js
@@ -3,10 +3,20 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello world');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
 });
 
 app.listen(PORT, () => {
