d05b62cf47fba2b0a5d416b92666d6136663ede5
Agil Adi Saputro <agiladisaputro@gmail.com>
2024-10-21 16:24:21 +0700

    [FIX] simple websocket

--- server.js
+++ server.js
@@ -3,22 +3,23 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello world');
-});
 
 io.on('connection', (socket) => {
   console.log('a user connected');
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+  socket.on('chat', (msg) => {
+    console.log('outgoing message: ' + msg);
+    io.emit('chat', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnexted');
   });
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
